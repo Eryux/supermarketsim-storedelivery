74f164955adf1c59677e242f1889c6fcdc5d232f
Eryux <ery.cnd@gmail.com>
2024-03-24 16:20:05 +0100

    Adds option to allow use of empty rack + pack assets in resources

--- StoreDelivery/Config.cs
+++ StoreDelivery/Config.cs
@@ -15,12 +15,18 @@ namespace StoreDelivery
 
         public ConfigEntry<float> ConfigPerBoxesCost { get; internal set; }
 
+        public ConfigEntry<bool> ConfigUseEmptyRackSlot { get; internal set; }
+
+        public ConfigEntry<bool> ConfigUseEmptyRackWithLabel { get; internal set; }
+
 
         public Config(ConfigFile cfg)
         {
             ConfigEnabled = cfg.Bind("StoreDelivery", "enabled", true, "Enable StoreDelivery Plugin");
             ConfigFeesEnabled = cfg.Bind("StoreDelivery", "fees_enables", true, "Apply fees when delivery is stored");
             ConfigPerBoxesCost = cfg.Bind("StoreDelivery", "fees_per_boxes", 3f, "Fees added for each product box in your delivery");
+            ConfigUseEmptyRackSlot = cfg.Bind("StoreDelivery", "use_empty_rack", false, "Allow the mod to place box on empty rack with no label");
+            ConfigUseEmptyRackWithLabel = cfg.Bind("StoreDelivery", "use_empty_rack_w_label", false, "Allow the mod to place box on empty rack even if its label doesn't match with the product");
         }
     }
 }
---
+++ StoreDelivery/RackTool.cs
@@ -0,0 +1,66 @@
+﻿// Copyright(c) 2024 - C.Nicolas <contact@bark.tf>
+// github.com/eryux
+
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using UnityEngine;
+
+namespace StoreDelivery
+{
+    public class RackTool
+    {
+        public static RackSlot GetRackSlotFor(Box box, bool allowEmpty = false, bool allowReplace = false)
+        {
+            if (box.HasProducts)
+            {
+                RackManager rackManager = MyBox.Singleton<RackManager>.Instance;
+
+                if (rackManager != null)
+                {
+                    List<Rack> racks = typeof(RackManager).GetField("m_Racks", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(rackManager) as List<Rack>;
+
+                    RackSlot slot = racks.SelectMany((Rack rack) => rack.RackSlots).FirstOrDefault((RackSlot s) => s.Data.ProductID == box.Product.ID && !s.Full);
+
+                    if (slot == null && allowEmpty)
+                    {
+                        slot = racks.SelectMany((Rack rack) => rack.RackSlots).FirstOrDefault((RackSlot s) => s.Data.ProductID < 1 && s.CurrentBoxID == -1 && !s.HasBox);
+                    }
+
+                    if (slot == null && allowReplace)
+                    {
+                        slot = racks.SelectMany((Rack rack) => rack.RackSlots).FirstOrDefault((RackSlot s) => !s.HasBox);
+                    }
+
+                    return slot;
+                }
+            }
+
+            return null;
+        }
+
+
+        public static void PlaceBoxInRack(RackSlot rackSlot, Box box)
+        {
+            Collider[] componentsInChildren = box.GetComponentsInChildren<Collider>();
+            for (int i = 0; i < componentsInChildren.Length; i++)
+            {
+                componentsInChildren[i].isTrigger = false;
+            }
+
+            Rigidbody boxPhysics;
+            if (box.TryGetComponent<Rigidbody>(out boxPhysics))
+            {
+                boxPhysics.isKinematic = true;
+                boxPhysics.velocity = Vector3.zero;
+                boxPhysics.interpolation = RigidbodyInterpolation.None;
+            }
+
+            box.gameObject.layer = LayerMask.NameToLayer("Interactable");
+
+            rackSlot.AddBox(box.BoxID, box);
+
+            box.Racked = true;
+        }
+    }
+}
